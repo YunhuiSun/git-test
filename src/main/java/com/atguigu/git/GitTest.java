6ece5bb5cdb1ce6124ca58339dfc1a7d823bfabf
YunhuiSun <sunyunhui1@163.com>
2025-08-30 17:46:34 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.atguigu.git
+ * Description:
+ *
+ * @Author sunyunhui
+ * @Create 2025/8/30 17:36
+ * Version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
